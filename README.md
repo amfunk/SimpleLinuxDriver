7e72a43e1a9aa663670bb226c272b31e602f36ad
Aidan Funk <afunk@tharrosgroup.com>
2024-06-18 20:06:22 -0600

    Added additional code for automatic device creation

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Simple Linux Driver
-Simple Linux driver
+
+Simple Linux driver that copies the string "Hello world from kernel mode!" to the user space.
+
+## Build
--- device_file.c
+++ device_file.c
@@ -16,60 +16,94 @@ static ssize_t device_file_read(
     , size_t count
     , loff_t *possition)
 {
-    printk( KERN_NOTICE "Simple-driver: Device file is read at offset = %i, read bytes count = %u\n"
-        , (int)*possition
-        , (unsigned int)count );
+  printk( KERN_NOTICE "Simple-driver: Device file is read at offset = %i, read bytes count = %u\n"
+      , (int)*possition
+      , (unsigned int)count );
 
-    if( *possition >= g_s_Hello_World_size )
-        return 0;
+  if( *possition >= g_s_Hello_World_size )
+    return 0;
 
-    if( *possition + count > g_s_Hello_World_size )
-        count = g_s_Hello_World_size - *possition;
+  if( *possition + count > g_s_Hello_World_size )
+    count = g_s_Hello_World_size - *possition;
 
-    if( copy_to_user(user_buffer, g_s_Hello_World_string + *possition, count) != 0 )
-        return -EFAULT;
+  if( copy_to_user(user_buffer, g_s_Hello_World_string + *possition, count) != 0 )
+    return -EFAULT;
 
-    *possition += count;
-    return count;
+  *possition += count;
+  return count;
 }
 
 /*===============================================================================================*/
 static struct file_operations simple_driver_fops = 
 {
-    .owner = THIS_MODULE,
-    .read = device_file_read,
+  .owner = THIS_MODULE,
+  .read = device_file_read,
 };
 
 static int device_file_major_number = 0;
 static const char device_name[] = "Simple-driver";
+static struct class *device_class;
+static struct device *device_struct;
+static dev_t device_number;
 
 /*===============================================================================================*/
 int register_device(void)
 {
-    int result = 0;
+  int result = 0;
 
-    printk( KERN_NOTICE "Simple-driver: register_device() is called.\n" );
+  printk( KERN_NOTICE "Simple-driver: register_device() is called.\n" );
 
-    result = register_chrdev( 0, device_name, &simple_driver_fops );
-    if( result < 0 )
-    {
-        printk( KERN_WARNING "Simple-driver:  can\'t register character device with errorcode = %i\n", result );
-        return result;
-    }
+  result = register_chrdev( 0, device_name, &simple_driver_fops );
+  if( result < 0 )
+  {
+    printk( KERN_WARNING "Simple-driver:  can\'t register character device with errorcode = %i\n", result );
+    return result;
+  }
 
-    device_file_major_number = result;
-    printk( KERN_NOTICE "Simple-driver: registered character device with major number = %i and minor numbers 0...255\n"
-        , device_file_major_number );
+  device_file_major_number = result;
+  printk( KERN_NOTICE "Simple-driver: registered character device with major number = %i and minor numbers 0...255\n"
+      , device_file_major_number );
 
-    return 0;
+  device_class = class_create( device_name );
+  if( IS_ERR( device_class ) )
+  {
+    printk( KERN_WARNING "Simple-driver: can't create device class with errorcode = %li\n", PTR_ERR( device_class) );
+    unregister_chrdev( device_file_major_number, device_name );
+    return PTR_ERR( device_class );
+  }
+  printk( KERN_NOTICE "Simple-driver: Device class created\n" );
+
+  device_number = MKDEV( device_file_major_number, 0 );
+  device_struct = device_create( device_class, NULL, device_number, NULL, device_name );
+  if ( IS_ERR( device_struct ) )
+  {
+    printk( KERN_WARNING "can't create device with errorcode = %li\n", PTR_ERR( device_struct ) );
+    class_destroy( device_class );
+    unregister_chrdev( device_file_major_number, device_name );
+    return PTR_ERR( device_struct );
+  }
+  printk( KERN_NOTICE "Simple-driver: Device created\n" );
+
+  return 0;
 }
 
 /*===============================================================================================*/
 void unregister_device(void)
 {
-    printk( KERN_NOTICE "Simple-driver: unregister_device() is called\n" );
-    if(device_file_major_number != 0)
-    {
-        unregister_chrdev(device_file_major_number, device_name);
-    }
+  printk( KERN_NOTICE "Simple-driver: unregister_device() is called\n" );
+  if ( !IS_ERR( device_class ) )
+  {
+    device_destroy( device_class, device_number );
+    printk( KERN_NOTICE "Simple-driver: Device destroyed\n" );
+  }
+  if ( !IS_ERR( device_class ) && !IS_ERR( device_struct ) )
+  {
+    class_destroy( device_class );
+    printk( KERN_NOTICE "Simple-driver: Device class destroyed\n" );
+  }
+  if(device_file_major_number != 0)
+  {
+    unregister_chrdev(device_file_major_number, device_name);
+    printk( KERN_NOTICE "Simple-driver: Device unregistered\n" );
+  }
 }
